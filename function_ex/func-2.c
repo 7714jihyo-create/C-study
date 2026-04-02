21031ee0491dee62a6e669a35068a9075d50f6c4
user <user@DESKTOP-8JQ4MJ8>
2026-04-02 12:49:23 +0900

    함수 실습

---
+++ function_ex/func-2.c
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+
+int func1();
+int func2();
+
+int main() {
+	printf("메인 시작\n"); //1
+	func1(); //2 함수호출
+	printf("메인 끝\n"); //8
+	return 0; 
+}
+
+int func1() {
+	printf("func1 함수 시작\n"); //3
+	func2(); //4
+	printf("func1 함수 끝\n"); //7
+	return 0;
+}
+int func2() {
+	printf("func2 함수 시작\n"); //5
+	printf("func2 함수 끝\n"); //6
+	return 0;
+}
---
+++ function_ex/recur.c
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+
+int func(int); //함수 선언 추가
+int main() {
+	int a;
+	printf("숫자를 입력하세요 : ");
+	scanf("%d", &a);
+	printf("%d", func(a)); //정수형 a를 매개변수 인자로 보내면서 func 함수 호출하여 받은 결과 출력
+}
+
+int func(int a) { //a=매개변수
+	if (a <= 1) {
+		return 1; //순환호출 멈추는 부분
+	}
+	return a * func(a - 1); //자기 자신을 순환적으로 호출하다가 결과 반환
+}
+
+/*
+a=5 5*func(4) = 5*24 = 120
+a=4 4*func(3) = 46 = 24
+a=3 3*func(2) = 3*2 = 6
+a=2 2*func(1) = 2*1 = 2
+*/
