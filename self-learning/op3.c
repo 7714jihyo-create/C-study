9afa386398c6998fac49cf2bf95ea8d635f2ad01
olzlkor <7714jihyo@gmail.com>
2026-05-28 16:15:22 +0900

    삼항연산자 (과제
    Added comment to clarify the use of the ternary operator.

--- self-learning/op3.c
+++ self-learning/op3.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-
+// 삼항연산자
 int main() {
 
 	int a = 10, b = 20;
 	int max;
 
-	max = a > b ? a : b;
-
+	max = a > b ? a : b; //a가 b보다 크면 max=a, 그렇지 않으면 max=b
+	printf("%d",max);
 }
